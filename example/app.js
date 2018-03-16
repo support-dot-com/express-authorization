4f57b813553e4928b29f035ae0a53baca5bed39b
Charles Eads <charles.eads@support.com>
2018-03-16 12:38:15 -0400

    update express example

--- example/app.js
+++ example/app.js
@@ -1,8 +1,5 @@
-/**
- * Module dependencies.
- */
-
 var express = require('express');
+var session = require('express-session');
 var http = require('http');
 var path = require('path');
 var authorization = require('../');
@@ -13,20 +10,12 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
 
 // setup permission middleware
 var ensureNounVerb = authorization.ensureRequest.isPermitted('noun:verb');
